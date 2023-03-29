16687449104573d1a930555c6bce28f102250f65
kumar ayush <ayushssshhh.com>
2023-03-29 20:13:33 +0530

    v: 1

---
+++ server.js
@@ -0,0 +1,56 @@
+//jshint esversion:6
+const express = require("express"); 
+const bodyParser = require("body-parser");
+const { OpenAIApi, Configuration } = require("openai");
+
+const app = express();
+
+// setting view folder
+app.set('view engine', 'ejs');
+
+//get info in consloe after posting
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+const port = 3000;
+const exampleUrl = ['./images/exapmle/1.png' , './images/exapmle/2.png' , './images/exapmle/3.png' , './images/exapmle/4.png' , './images/exapmle/5.png' , './images/exapmle/6.png'];
+let imgUrl = [];  
+OPENAI_API_KEY='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'
+
+const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+
+app.get("/" , (req , res)=>{
+    res.render("home" , {
+        titleRender : "Some IMAGE AI Generated Results",
+        imageRender : exampleUrl
+    });
+})
+
+app.post("/" , async (req , res)=>{
+    console.log("posted");
+    imgUrl = [];
+
+    var prompt = req.body.search;
+        const result = await openai.createImage({
+            prompt,
+            n: 5,
+            size: "512x512"
+        });
+        
+        for(var i=0 ; i<5 ; i++){
+            imgUrl.push(result.data.data[i].url);
+        }
+
+    res.render("home" , {
+        titleRender : prompt,
+        imageRender : imgUrl,
+    })
+})
+
+app.listen(port, ()=>{
+    console.log("server started on port " + port);
+})
